751251cbefdc2999e601e87738c345e5cdf91694
renish-oyo <renish.singh@oyorooms.com>
2020-10-22 09:47:20 +0545

    Hi

--- src/main/java/com/example/miniproject/controller/FirstController.java
+++ src/main/java/com/example/miniproject/controller/FirstController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class FirstController {
     @GetMapping("/")
     public String home(){
-        return "Hello World!";
+        return "Hi ";
     }
 
 }
