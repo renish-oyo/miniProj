dfb3e26ff8cd2ace598f7d998e6b917a65fc6d37
renish-oyo <renish.singh@oyorooms.com>
2020-10-22 08:55:48 +0545

    regex

--- src/main/java/com/example/miniproject/MiniprojectApplication.java
+++ src/main/java/com/example/miniproject/MiniprojectApplication.java
@@ -1,9 +1,7 @@
 package com.example.miniproject;
 
-import com.example.miniproject.Controller.FirstController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class MiniprojectApplication {
--- src/main/java/com/example/miniproject/controller/FirstController.java
+++ src/main/java/com/example/miniproject/controller/FirstController.java
@@ -1,4 +1,4 @@
-package com.example.miniproject.Controller;
+package com.example.miniproject.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
