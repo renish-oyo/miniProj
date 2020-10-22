55d5850c01f032056e5e672af3d61ba4aa9f293a
renish-oyo <renish.singh@oyorooms.com>
2020-10-22 08:44:25 +0545

    home api added

--- src/main/java/com/example/miniproject/Controller/FirstController.java
+++ src/main/java/com/example/miniproject/Controller/FirstController.java
@@ -1,8 +1,13 @@
 package com.example.miniproject.Controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FirstController {
+    @GetMapping("/")
+    public String home(){
+        return "Hello World!";
+    }
 
 }
