0432815c0e53ef11facb99b71263b833489853e4
renish-oyo <renish.singh@oyorooms.com>
2020-10-29 16:18:50 +0545

    Cleaned

--- src/main/java/com/example/miniproject/models/entity/User.java
+++ src/main/java/com/example/miniproject/models/entity/User.java
@@ -1,9 +1,6 @@
 package com.example.miniproject.models.entity;
-
 import lombok.*;
-
 import javax.persistence.*;
-import javax.validation.constraints.NotEmpty;
 import java.io.Serializable;
 
 @Getter
