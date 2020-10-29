d0dd275b4795f542a9580094ed1f08c2f7c54cd7
renish-oyo <renish.singh@oyorooms.com>
2020-10-29 11:10:35 +0545

    Field Injection is changed to Constructor Injection

--- src/main/java/com/example/miniproject/configuration/SecurityConfiguration.java
+++ src/main/java/com/example/miniproject/configuration/SecurityConfiguration.java
@@ -8,6 +8,6 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.
+
     }
 }
