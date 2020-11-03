979e1def2275802b26b7eb55e29d287f4268acb3
renish-oyo <renish.singh@oyorooms.com>
2020-11-03 11:05:47 +0545

    @Transactional added in UserRepo

--- src/main/java/com/example/miniproject/repository/UserRepository.java
+++ src/main/java/com/example/miniproject/repository/UserRepository.java
@@ -3,10 +3,12 @@ package com.example.miniproject.repository;
 import com.example.miniproject.models.entity.User;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Repository
+@Transactional
 public interface UserRepository extends CrudRepository<User,Integer> {
     User findByEmail(String email);
     Boolean existsByEmail(String email);
